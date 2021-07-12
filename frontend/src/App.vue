872f7cebe1752bd18f8bacea2b37beca012e866e
partrickJhonson <74884276+partrickJhonson@users.noreply.github.com>
2021-07-11 21:33:02 -0300

    incluindoCampos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,34 +3,110 @@
 
     <nav>
       <div class="nav-wrapper blue darken-1">
-        <a href="#" class="brand-logo center">Produtos Front</a>
+        <a href="//" class="brand-logo center">Produtos Front</a>
       </div>
     </nav>
 
     <div class="container">
-      <form @submit.prevent="salvar"> 
- 
-          <label>Titulo</label>
-          <input type="text" placeholder="Titulo" v-model="objeto.titulo">
+      <form @submit.prevent="salvar">         
+        <h2>Dados Gerais</h2>
+        <hr>
+        <label>Titulo</label>
+        <input type="text" placeholder="Titulo"  v-model="objeto.titulo" >
+        <label>Catalogo</label>
+        <input type="text" placeholder="Caralogo" v-model="objeto.catalogo">
 
-          <label>Autor</label>
-          <input type="text" placeholder="Autor" v-model="objeto.autor">
+        <label>Língua</label>
+        <input type="text" placeholder="Lingua" v-model="objeto.lingua">
+
+        <label>Descrisão</label>
+        <input type="text" placeholder="Descrisão" v-model="objeto.descrisao">
+
+        <label>palavra Chave</label>
+        <input type="text" placeholder="palavra chave" v-model="objeto.palavrachave">
+
+        <label>Cobertura</label>
+        <input type="text" placeholder="Cobertura" v-model="objeto.corbertura">
+
+        <label>Estrtura</label>
+        <input type="text" placeholder="Estrtura" v-model="objeto.estrtura">
+
+        <label>Nivel de Agregação</label>
+        <input type="text" placeholder="Nivel de Agregação" v-model="objeto.niveldeAgregacao">
 
 
-          <label>ano_publicacao</label>
-          <input type="number" placeholder="ano_publicacao" v-model="objeto.ano_publicacao">
+        <h2>Ciclo de Vida</h2>
+        <hr>
+        <label>Versao</label>
+        <input type="text" placeholder="Versao" v-model="objeto.Versao">
 
-          <label>estado</label>
-          <input type="text" placeholder="estado" v-model="objeto.estado">
+        <label>Status</label>
+        <input type="text" placeholder="Status" v-model="objeto.Status">
 
-          <label>Npaginas</label>
-          <input type="text" placeholder="Npaginas" v-model="objeto.Npaginas">
+        <label>Funcao do Contribuinte</label>
+        <input type="text" placeholder="Funcao do Contribuinte" v-model="objeto.ContribuinteFuncao">
 
-          <label>editora</label>
-          <input type="text" placeholder="editora" v-model="objeto.editora">
+        <label>Entidada do Contribuinte</label>
+        <input type="text" placeholder="Entidada do Contribuinte" v-model="objeto.ContribuinteEntidadae">
 
-          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+        <label>Data da  Contribuição</label>
+        <input type="text" placeholder="Data da  Contribuição" v-model="objeto.ContribuinteData">
 
+        <h2>Meta-Metadados</h2>
+        <hr>
+        <label>identificador</label>
+        <input type="text" placeholder="identificador" v-model="objeto.identificador">
+
+        <label>Catalogo</label>
+        <input type="text" placeholder="Catalogo" v-model="objeto.catalogoMeta">
+
+        <label>Catalogo de Entrada</label>
+        <input type="text" placeholder="catalogo de Entrada" v-model="objeto.catalogoMetaEntrada">
+
+        <label>Data Entrada</label>
+        <input type="text" placeholder="Data Entrada" v-model="objeto.catalogoMetaData">
+
+        <label>Esquema</label>
+        <input type="text" placeholder="Esquema" v-model="objeto.EsquemaMeta">
+
+        <label>Idioma</label>
+        <input type="text" placeholder="Idioma" v-model="objeto.idiomameta">
+
+        <h2>Técnico</h2>
+        <hr>
+        <label>formato</label>
+        <input type="text" placeholder="formato" v-model="objeto.formato">
+
+        <label>Tamanho</label>
+        <input type="text" placeholder="tamanho" v-model="objeto.tamanho">
+
+        <label>Localização</label>
+        <input type="text" placeholder="Localização" v-model="objeto.localizacao">
+
+        <label>Tipo de Requisito</label>
+        <input type="text" placeholder="Tipo de Requisito" v-model="objeto.requisitoTipo">
+
+        <label>Nome do Requisito</label>
+        <input type="text" placeholder="Nome do Requisito" v-model="objeto.requisitoNome">
+
+        <label>Observação</label>
+        <input type="text" placeholder="Observação" v-model="objeto.Observacao">
+
+        <label>Outros Requitos</label>
+        <input type="text" placeholder="Outros Requisitos" v-model="objeto.outrosRequitos">
+
+        <label>duracao</label>
+        <input type="text" placeholder="duracao" v-model="objeto.duracao">
+
+        <label>x1</label>
+        <input type="text" placeholder="x1" v-model="objeto.x1">
+
+
+
+
+      
+
+            <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
       </form>
 
       <table>
@@ -76,12 +152,72 @@ export default{
     return{
       objetos:[],
       objeto:{
-        titulo:'',
-        autor:'',
-        ano_pulicacao:0,
-        estado:' ',
-        Npaginas:'',
-        editora:''        
+    titulo:'.',
+    catalogo:'.',
+    lingua:'null',
+    descrisao:'null',
+    palavrachave:'null',
+    corbertura:'null',
+    estrtura:'null',
+    niveldeAgregacao:0,
+//Ciclo de Vida
+    Versao:'.',
+    Status:'.',
+    ContribuinteFuncao:'.',
+    ContribuinteEntidadae:'.',
+    ContribuinteData:'.',
+//Meta-Metadados
+    identificador:'.',
+    catalogoMeta:'.',
+    catalogoMetaEntrada:'.',
+    catalogoMetaData:'.',
+    EsquemaMeta:'.',
+    idiomameta:'.',
+//Tecnico
+    formato:'.',
+    tamanho:'.',
+    localizacao:'.',
+    requisitoTipo:'.',
+    requisitoNome:'.',
+    Observacao:'.',
+    outrosRequitos:'.',
+    duracao:'.',
+//Educaional
+    tipodeinteratividade:'.',
+    tipodeRecursodeAprendizagem:'.',
+    niveldeinteratividade:'.',
+    densidadesemantica:'.',
+    funcaodousuariofinalpretendido:'.',
+    contexto:'.',
+    faixaEtaria:'.',
+    dificuldade:'.',
+    TempodeAprendizagem:'.',
+    DescrisaoEducacional:'.',
+    idioma:'.',
+//Direitos
+    custo:'.',
+    DireitosautoraisRestricoes:'.',
+    DescrisaoDireitos:'.',
+//Relacao
+    tipoRelacao:'.',
+    recursoRelacao :'.',
+//Anotação
+    pessoaAnotacao:'.',
+    dataanotacao:'.',
+    descrisaoanotacao:'.',
+//Clasificao
+    closificacaoObjetivo:'.',
+    taxon:'.',
+    taxonFonte:'.',
+    taxonId:'.',
+    taxonEntrada:'.',
+    classificacaoDescrisao:'.',
+    PalavraPasseClassificacao:'.',
+    autor:'.',
+    ano_pulicacao:0,
+    estado:'.',
+    Npaginas:0,
+    editora:'.', 
       },
 
     }
@@ -97,9 +233,8 @@ export default{
     salvar(){
       objeto.salvar(this.objeto)
       // then(resposta =>{
-      //   alert('Salvo com Sucesso')
-      // })
-      
+      //   alert('Salvo com Sucesso'+resposta.log)
+      // }).catch(alert('Erro as Salvar')+Error)     
 
     }
   }
