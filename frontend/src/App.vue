7353c80694fdaaa6d8a9eeb74866b1199f09fcd2
partrickJhonson <74884276+partrickJhonson@users.noreply.github.com>
2021-07-11 22:18:23 -0300

    Finalizando campos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -98,15 +98,106 @@
         <label>duracao</label>
         <input type="text" placeholder="duracao" v-model="objeto.duracao">
 
-        <label>x1</label>
-        <input type="text" placeholder="x1" v-model="objeto.x1">
+        <h2>Educaional</h2>
+        <hr>
+        <label>Interatividade</label>
+        <input type="text" placeholder="Interatividade" v-model="objeto.tipodeinteratividade">
+        
+        <label>Recurso de Aprendizagem</label>
+        <input type="text" placeholder="Recurso de Apendizagem" v-model="objeto.tipodeRecursodeAprendizagem">
 
+        <label>nível de interatividade</label>
+        <input type="text" placeholder="nível de interatividade" v-model="objeto.niveldeinteratividade">
 
+        <label>Densidade Semântica</label>
+        <input type="text" placeholder="Densidade Semântica" v-model="objeto.densidadesemantica">
 
+        <label>Usuário Pretendido</label>
+        <input type="text" placeholder="Usuário Pretendido" v-model="objeto.funcaodousuariofinalpretendido">
 
-      
+        <label>Contexto</label>
+        <input type="text" placeholder="Contexto" v-model="objeto.contexto">
 
-            <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+        <label>Faixa Etaria</label>
+        <input type="text" placeholder="Faixa Etaria" v-model="objeto.faixaEtaria">
+
+        <label>dificuldade</label>
+        <input type="text" placeholder="dificuldade" v-model="objeto.dificuldade">
+
+        <label>Tempo de Apredizagem</label>
+        <input type="text" placeholder="Tempo de Apredizagem" v-model="objeto.TempodeAprendizagem">
+
+        <label>idioma</label>
+        <input type="text" placeholder="idioma" v-model="objeto.idioma">
+
+        <h2>Direitos</h2>
+        <hr>
+        <label>custo</label>
+        <input type="text" placeholder="custo" v-model="objeto.custo">
+
+        <label>Direitos e Restrições</label>
+        <input type="text" placeholder="Direitos e Restrições" v-model="objeto.DireitosautoraisRestricoes">
+
+        <label>Descrisão</label>
+        <input type="text" placeholder="Descrisão" v-model="objeto.DescrisaoDireitos">
+
+        <h2>Relação</h2>
+        <hr>
+        <label>Tipo</label>
+        <input type="text" placeholder="Tipo" v-model="objeto.tipoRelacao">
+
+        <label>Recurso</label>
+        <input type="text" placeholder="Recurso" v-model="objeto.recursoRelacao">
+
+        <h2>Anotação</h2>
+        <hr>
+        <label>Pessoa Que fez Anotação</label>
+        <input type="text" placeholder="Pessoa" v-model="objeto.pessoaAnotacao">
+
+        <label>Data da Anotação</label>
+        <input type="text" placeholder="Data da Anotação" v-model="objeto.dataanotacao">
+
+        <label>Descrisão</label>
+        <input type="text" placeholder="Descrisão" v-model="objeto.descrisaoanotacao">
+
+        <h2>Classificação</h2>
+        <hr>
+        <label>Objetivo</label>
+        <input type="text" placeholder="Objetivo" v-model="objeto.closificacaoObjetivo">
+
+        <label>Taxon</label>
+        <input type="text" placeholder="Taxon" v-model="objeto.taxon">
+
+        <label>Fonte Taxon</label>
+        <input type="text" placeholder="Fonte Taxon" v-model="objeto.taxonfonte">
+
+        <label>Id Taxon</label>
+        <input type="text" placeholder="Id Taxon" v-model="objeto.taxonId">
+
+        <label>Taxon Entrada</label>
+        <input type="text" placeholder="Taxon Entrada" v-model="objeto.taxonEntrada">
+
+        <label>Descrisão da Classificação</label>
+        <input type="text" placeholder="Descrisão da Classificação" v-model="objeto.classificacaoDescrisao">
+
+        <label>Palavra Passe</label>
+        <input type="text" placeholder="Palavra Passe" v-model="objeto.PalavraPasseClassificacao">
+
+        <label>Autor</label>
+        <input type="text" placeholder="Autor" v-model="objeto.autor">
+
+        <label>Ano da Publicação</label>
+        <input type="text" placeholder="Ano da Publicação" v-model="objeto.ano_pulicacao">
+
+        <label>estado</label>
+        <input type="text" placeholder="estado" v-model="objeto.estado">
+
+        <label>N° Páginas</label>
+        <input type="text" placeholder="N° Páginas" v-model="objeto.Npaginas">
+
+        <label>editora</label>
+        <input type="text" placeholder="editora" v-model="objeto.editora">
+        <button class="waves-effect waves-light btn-small">Salvar</button>
       </form>
 
       <table>
@@ -130,8 +221,7 @@
             <td>{{ objeto.titulo }}</td>
             <td>{{ objeto.Npaginas }}</td>
             <td>
-              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
-              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+              <button class="waves-effect btn-small red darken-1">Excluir</button>
             </td>
 
           </tr>
