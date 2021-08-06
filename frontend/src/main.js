b59d75e3d7d12cc8a9566e07cb844fbbd06c6698
partrickJhonson <patrick.jhonsom@ufvjm.edu.br>
2021-08-06 01:46:32 -0300

    Rotas_Frontend
    Iniciando as rotas no frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Cadastro from './components/Cadastro'
+import Index    from './components/Index'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        {path: '/Cadastro',component: Cadastro},
+        {path: '/index'   ,component: Index},
+        {path: '/'        ,redirect : Index}//Direciona o user para a rota inicial
+    ]
+
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+    router,
     render: h => h(App),
 }).$mount('#app')
